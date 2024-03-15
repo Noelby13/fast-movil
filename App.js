63cafb71617f073b172fe221b67e36c0ea2fc6f3
Noelby13 <hnoelreyes25@gmail.com>
2024-03-15 09:44:32 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { Text,StyleSheet, View} from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StartScreen } from "./src/screens/StartScreen";
+
+const Stack = createStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="StartScreen" screenOptions={{headerShown:false}}>
+          <Stack.Screen name="StartScreen" component={StartScreen}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+
   );
 }
 
---
+++ src/components/BackGround.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { KeyboardAvoidingView, StyleSheet} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export const BackGround = ({ children }) => {
+  return (
+    <SafeAreaView style ={styles.safeContainer}> 
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+      { children }
+      </KeyboardAvoidingView>
+
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    safeContainer:{
+        flex: 1,
+        width: '100%',
+        backgroundColor: "#121223"
+    },
+    container:{
+        flex: 1,
+        padding:20,
+        width: '100%',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    }
+
+})
